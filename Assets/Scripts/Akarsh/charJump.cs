93b2b1316e23774a440041a39ca8720cc3ed535f
akarsh <a.kumar@student.saedubai.com>
2021-06-25 19:22:34 +0400

    added player controller

---
+++ Assets/Scripts/Akarsh/charJump.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charJump : MonoBehaviour
+{
+    public float jumpHeight = 6f;
+    public bool isGrounded;
+
+    private new Rigidbody rigidbody; 
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                rigidbody.AddForce(Vector3.up * jumpHeight);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+    }
